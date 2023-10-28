099de04aed3b41b5d6b1f85f086f8563e33eda91
Seasawher <tellagoodstory@icloud.com>
2023-10-28 20:51:01 +0900

    baseurl をセットする

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "Seasawher.github.io/math-roadmap",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
